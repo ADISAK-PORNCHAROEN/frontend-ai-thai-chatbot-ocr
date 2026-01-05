fc1793242f0a62496f3343f6dedd4d2f20d4be72
ADISAK-PORNCHAROEN <flukekub654@gmail.com>
2026-01-06 01:11:13 +0700

    add cloudflare domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: ['chatbot.archonnode.com']
+  },
+  server: {
+    allowedHosts: ['chatbot.archonnode.com']
+  }
 })
